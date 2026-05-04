5796ce4021c05a83e97df492a790e0e1c3c3d8d9
Martin Sieniawski <m.sieniawski@live.ca>
2026-05-03 19:41:49 -0700

    Day 2

--- years/2015/src/days/2.ts
+++ years/2015/src/days/2.ts
@@ -6,7 +6,7 @@ export function part_one(input: string): number {
         let rowResult = 0;
         const dimensions: number[] = _.map(row.split("x"), (d) => Number(d));
 
-        if (dimensions && dimensions[0] && dimensions[1] && dimensions[2]) {
+        if (dimensions[0] && dimensions[1] && dimensions[2]) {
             const areas = [
                 dimensions[0] * dimensions[1],
                 dimensions[1] * dimensions[2],
@@ -15,7 +15,7 @@ export function part_one(input: string): number {
             rowResult = _.reduce([
                 ..._.map(areas, (area) => { return area * 2 }),
                 Math.min(...areas),
-            ], (accumulator, dimension): number => { return accumulator += dimension }, 0);
+            ], (accumulator, dimension): number => accumulator += dimension, 0);
         }
         return sum += rowResult;
     });
@@ -23,5 +23,22 @@ export function part_one(input: string): number {
     return sum;
 }
 export function part_two(input: string): number {
-    return 0;
+    let sum = 0;
+
+    _.each(input.split("\n"), (row) => {
+        let rowResult = 0;
+        const dimensions: number[] = _.map(row.split("x"), (d) => Number(d)).sort((a, b) => a - b);
+
+        if (dimensions[0] && dimensions[1] && dimensions[2]) {
+            rowResult = _.reduce([
+                2 * dimensions[0],
+                2 * dimensions[1],
+                dimensions[0] * dimensions[1] * dimensions[2],
+            ], (accumulator, dimension) => accumulator += dimension, 0)
+        }
+
+        sum += rowResult;
+    });
+
+    return sum;
 }
